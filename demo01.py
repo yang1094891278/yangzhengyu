d2b8db150bb7681ec1371a0ef299cecc9369b32c
yang1094891278 <yang1094891278@163.com>
2022-02-17 15:12:15 +0800

    第一次尝试

---
+++ demo01.py
@@ -0,0 +1,45 @@
+#python的常见类型
+# print("hello world!")   #字符串 str
+# print(233)   #整数 int
+# print(1.2)   #小数 float
+# print(True)   #布尔值 True False 
+# print(())     #元组 tuple
+# print([])    #数组 list
+# print({})    #字典 dict
+#空 None
+
+# print("哈哈哈",2333,1.2)  #用逗号隔开
+# print("哈哈哈"+"嘻嘻嘻")
+# print("哈"*10)
+# print(1+55)
+# print(3>2)
+# print(1+1==3)
+
+
+# #变量  赋值
+# a = input("请输入:")
+# print("输出:",a)
+
+
+# #type 读取数据的类型
+# print(type(1.2),type("你好"),type(2),type(True),type(()),type([]),type({}))
+
+# #数据类型转换
+#规律  任何数据都可以转换成字符串  除了空None
+#小数和整数可以互相转换
+#字符串转换其他类型数据 必须满足长得像的规律
+# a=int("2333")
+# print(type(a))
+
+# a = float (input("请输入："))
+# b = float (input("请输入："))
+# print("输出：",a+b)
+
+# #计算字符串的长度
+# a = "diuaidha udahd"
+# print(len(a))
+
+# #练习 
+# a = input("请输入:")
+# b = input("请输入:")
+# print("a+b的长度:",len(a)+len(b))
