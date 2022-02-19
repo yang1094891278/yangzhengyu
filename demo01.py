88061290712e7f186158baf5da1c9fabd8444d1f
yang1094891278 <yang1094891278@163.com>
2022-02-19 16:06:23 +0800

    提交

--- demo01.py
+++ demo01.py
@@ -52,7 +52,7 @@
 # print(a.count(2))
 # #下标是就近原则 显示   index()里面只能有一个值
 # print(a.index(3)) 
-#切片  同时取多个值  左闭右开  
+# #切片  同时取多个值  左闭右开  
 # print(a[0:5])
 
 # #二维元组
@@ -64,35 +64,60 @@
 # print(b[0][1])
 
 
-#元组和数组的区别 一旦写好后元组不能修好 数组可以修改
-#数组
-a =  [1,2,3,"你好",True,2,3]
-print(a[4])
-#方法
-#1、追加数据  在尾巴上追加
-a.append("append")
-print(a)
-#2、count（）统计某个值个数和index（）查找某个值下标
-#下标是就近原则 显示   index()里面只能有一个值
-print(a.count(2))
-print(a.index(3))
-#往数组中准确的插入数据 insert（位置，数据）
-a.insert(0,"insert")
-print(a)
-#类似于剪切的作用 吧值从数组中拿出来去别的地方用 pop()
-b =  [1,2,3,"你好",True,2,3]
-print(b)
-c = b.pop(3)
-print(b)
-print(c)
-#清空数据 clear()
-b.clear()
-print(b)
-#数组往数组里面装  extend（）
-xx = ["哈哈",123]
-b.extend(xx)
-print(b)
-#可以直接print（xx+b）
-#移除 remove（值）
-b.remove(123)
-print(b)
+# #元组和数组的区别 一旦写好后元组不能修好 数组可以修改
+# #数组
+# a =  [1,2,3,"你好",True,2,3]
+# print(a[4])
+# #方法:
+# #1、追加数据  在尾巴上追加
+# a.append("append")
+# print(a)
+# #2、count（）统计某个值个数和index（）查找某个值下标
+# #下标是就近原则 显示   index()里面只能有一个值
+# print(a.count(2))
+# print(a.index(3))
+# #3、往数组中准确的插入数据 insert（位置，数据）
+# a.insert(0,"insert")
+# print(a)
+# #4、类似于剪切的作用 吧值从数组中拿出来去别的地方用 pop()
+# b =  [1,2,3,"你好",True,2,3]
+# print(b)
+# c = b.pop(3)
+# print(b)
+# print(c)
+# #5、清空数据 clear()
+# b.clear()
+# print(b)
+# #6、数组往数组里面装  extend（）
+# xx = ["哈哈",123]
+# b.extend(xx)
+# print(b)
+# #可以直接print（xx+b）
+# #移除 remove（值）
+# b.remove(123)
+# print(b)
+
+# #字典
+# '''
+# 特点：
+# 1、字典中的值没有顺序 
+# 2、字典的结构必须是键值对（key：value） 的结构
+# '''
+# a = {"name":"张三","age":25}
+# #取值a[key]
+# print(a["name"])
+# #新增 
+# a["high"] = "175cm"
+# #修改
+# a["name"]= "李四"
+# print(a)
+# #方法：
+# #取值 get（） 取值不存在时 返回空值None
+# b = a.get("name")
+# print(b)
+# #剪切 pop（）
+# c = a.pop("high")
+# print(a)
+# #更新 update()
+# a.update(name = 123)
+# print(a)
